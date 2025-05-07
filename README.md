d824b4a8729158a31b0b7a6957790e2c1e79f38a
Phillip <phillipthomas@live.de>
2025-05-07 14:05:48 +0200

    gemma3 wrapper for image question answering

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Provider Endpoint Template for Python
+# Gemma 3
+This is a ollama based wrapper for the [Gemma 3](https://ai.google.dev/gemma/) model.
+
+Gemma is a lightweight, family of models from Google built on Gemini technology. The Gemma 3 models are multimodal—processing text and images—and feature a 128K context window with support for over 140 languages. Available in 1B, 4B, 12B, and 27B parameter sizes, they excel in tasks like question answering, summarization, and reasoning, while their compact design allows deployment on resource-limited devices.
+
+![Gemma 3](https://ollama.com/assets/library/gemma3/89dc5a19-179e-4dd3-8e5d-12ad54973148)
+
+## Gemma Terms of Use
+https://ai.google.dev/gemma/terms
--- app/main.py
+++ app/main.py
@@ -1,27 +1,55 @@
+import logging
 from datetime import datetime, timezone
 
+from make87_messages.core.empty_pb2 import Empty
 from make87_messages.core.header_pb2 import Header
+from make87_messages.image.compressed.image_jpeg_pb2 import ImageJPEG
 from make87_messages.text.text_plain_pb2 import PlainText
 import make87
+from ollama import chat, Client, Message, Image
+
+logger = logging.getLogger(__name__)
 
 
 def main():
     make87.initialize()
     endpoint = make87.get_provider(
-        name="EXAMPLE_ENDPOINT", requester_message_type=PlainText, provider_message_type=PlainText
+        name="IMAGE_QUESTION", requester_message_type=PlainText, provider_message_type=PlainText
     )
+    image_getter = make87.get_requester(
+        name="GET_IMAGE", requester_message_type=Empty, provider_message_type=ImageJPEG
+    )
+    client = Client()
 
     def callback(message: PlainText) -> PlainText:
-        received_dt = datetime.now(tz=timezone.utc)
-        publish_dt = message.header.timestamp.ToDatetime().replace(tzinfo=timezone.utc)
-        print(
-            f"Received message '{message.body}'. Sent at {publish_dt}. Received at {received_dt}. Took {(received_dt - publish_dt).total_seconds()} seconds."
-        )
 
-        return PlainText(
-            header=make87.header_from_message(Header, message=message, append_entity_path="response"),
-            body=message.body[::-1],
-        )
+        try:
+            image = image_getter.request(message=Empty(), timeout=30)
+
+            response = client.chat(
+                model="gemma3",
+                messages=[
+                    Message(
+                        role="user",
+                        content=message.body,
+                        images=[Image(
+                            value=image.data
+                        )]
+                    )
+                ],
+                options={"temperature": 0},  # Set temperature to 0 for more deterministic output
+            )
+            return PlainText(
+                header=make87.header_from_message(Header, message=message, append_entity_path="response"),
+                body=response.message.content,
+            )
+
+        except Exception as e:
+            logger.error(f"Error processing message: {e}")
+            return PlainText(
+                header=make87.header_from_message(Header, message=message, append_entity_path="response"),
+                body=f"Error processing message: {e}",
+            )
 
     endpoint.provide(callback)
     make87.loop()
