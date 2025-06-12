7ccea6392c87994f1544bfafbe0ee7fedf37250a
PhillipThomas1992 <phillipthomas@live.de>
2025-06-12 16:48:16 +0200

    added mcp server

--- app/main.py
+++ app/main.py
@@ -1,58 +1,56 @@
 import logging
-from datetime import datetime, timezone
 
+import mcp
+from make87.interfaces.zenoh import ZenohInterface
 from make87_messages.core.empty_pb2 import Empty
-from make87_messages.core.header_pb2 import Header
 from make87_messages.image.compressed.image_jpeg_pb2 import ImageJPEG
-from make87_messages.text.text_plain_pb2 import PlainText
-import make87
+from make87.encodings import ProtobufEncoder
+import make87 as m87
 from ollama import chat, Client, Message, Image
 
 logger = logging.getLogger(__name__)
 
 
 def main():
-    make87.initialize()
-    endpoint = make87.get_provider(
-        name="IMAGE_QUESTION", requester_message_type=PlainText, provider_message_type=PlainText
-    )
-    image_getter = make87.get_requester(
-        name="GET_IMAGE", requester_message_type=Empty, provider_message_type=ImageJPEG
-    )
+    config = m87.config.load_config_from_env()
+    zenoh_interface = ZenohInterface(name="zenoh-client", make87_config=config)
+    image_getter = zenoh_interface.get_requester("GET_IMAGE")
     client = Client()
 
-    def callback(message: PlainText) -> PlainText:
+    server = mcp.server.FastMCP(name="image_describer", host="0.0.0.0", port=9988)
+
+    @server.tool(description="Ask a question with respect to the current image captured by the camera.")
+    def ask_question_about_camera_frame(message: str) -> str:
 
         try:
-            image = image_getter.request(message=Empty(), timeout=30)
+            response = image_getter.get(payload=ProtobufEncoder(message_type=Empty).encode(Empty()))
+            for r in response:
+                if r.ok is not None:
+                    image = ProtobufEncoder(message_type=ImageJPEG).decode(r.ok.payload.to_bytes())
 
-            response = client.chat(
-                model="gemma3",
-                messages=[
-                    Message(
-                        role="user",
-                        content=message.body,
-                        images=[Image(
-                            value=image.data
-                        )]
+                    response = client.chat(
+                        model="gemma3",
+                        messages=[
+                            Message(
+                                role="user",
+                                content=message,
+                                images=[Image(
+                                    value=image.data
+                                )]
+                            )
+                        ],
+                        options={"temperature": 0},  # Set temperature to 0 for more deterministic output
                     )
-                ],
-                options={"temperature": 0},  # Set temperature to 0 for more deterministic output
-            )
-            return PlainText(
-                header=make87.header_from_message(Header, message=message, append_entity_path="response"),
-                body=response.message.content,
-            )
+                    return response.message.content
+                else:
+                    return str(r.err)
 
         except Exception as e:
             logger.error(f"Error processing message: {e}")
-            return PlainText(
-                header=make87.header_from_message(Header, message=message, append_entity_path="response"),
-                body=f"Error processing message: {e}",
-            )
+            return f"Error processing message: {e}"
 
-    endpoint.provide(callback)
-    make87.loop()
+
+    server.run(transport="streamable-http")
 
 
 if __name__ == "__main__":
